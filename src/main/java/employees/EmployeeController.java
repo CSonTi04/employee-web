c0fc3c9c71e264f1a3e7e8649a20df8fd2c19920
Tóth András <tibor.andras.toth@idomsoft.hu>
2025-11-24 14:35:35 +0100

    Enhance EmployeeController to return ResponseEntity for findById method and add test case for not found scenario

--- src/main/java/employees/EmployeeController.java
+++ src/main/java/employees/EmployeeController.java
@@ -1,6 +1,7 @@
 package employees;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,10 @@ public class EmployeeController {
     }
 
     @GetMapping("/{id}")
-    public Mono<EmployeeDto> findById(Long id) {
-        return employeeService.findById(id);
+    public Mono<ResponseEntity<EmployeeDto>> findById(Long id) {
+        return employeeService
+                .findById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
